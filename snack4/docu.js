c5ea9d727da2f516ff866f2c95bfebf5cbf8d5b2
EdoardoSquillaci <edoardosquillaci@gmail.com>
2024-05-16 20:59:29 +0200

    Fine

--- snack4/docu.js
+++ snack4/docu.js
@@ -5,12 +5,10 @@ const invitati = ['giorgio' , 'luca' , 'giovanni' , 'marco']
 let richiesta = prompt('Inserisci il tuo nome')
  
 
-
-
-if(richiesta = invitati){
+if(invitati.includes(richiesta)){
 
     console.log('puoi venire alla festa')
 
-} else if (richiesta != invitati) {
+} else {
  console.log('Non puoi entrare alla festa')
 }
