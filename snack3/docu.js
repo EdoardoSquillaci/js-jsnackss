c9e2136937a1e3cc8f50948b553cb7311eced25b
EdoardoSquillaci <edoardosquillaci@gmail.com>
2024-05-16 20:59:10 +0200

    Fine snack 4

---
+++ snack3/docu.js
@@ -0,0 +1,11 @@
+
+let somma= 0
+
+
+for (let i = 0; i <= 10; i++){
+    
+    somma += prompt('Inserisci un Numero')
+
+
+    console.log(i)
+}
---
+++ snack4/docu.js
@@ -0,0 +1,16 @@
+
+
+const invitati = ['giorgio' , 'luca' , 'giovanni' , 'marco'] 
+
+let richiesta = prompt('Inserisci il tuo nome')
+ 
+
+
+
+if(richiesta = invitati){
+
+    console.log('puoi venire alla festa')
+
+} else if (richiesta != invitati) {
+ console.log('Non puoi entrare alla festa')
+}
